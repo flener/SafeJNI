b8b328918dc523ca529904591a8456390d5ea37f
Fernando Lener <nando0205@gmail.com>
2017-06-07 17:41:57 -0300

    Making filepaths relative to LOCAL_PATH

--- src/jni/Android.mk
+++ src/jni/Android.mk
@@ -1,12 +1,15 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
-	../safejni.cpp
+	$(LOCAL_PATH)/../safejni.cpp
 LOCAL_CPPFLAGS := \
 	-frtti \
 	-fexceptions \
 	-std=c++11 \
 	-D__GXX_EXPERIMENTAL_CXX0X__
+
 LOCAL_LDLIBS := -llog -latomic
 LOCAL_MODULE := safejni
+
 include $(BUILD_SHARED_LIBRARY)
